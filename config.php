730c8bedbae18cee8754c9a3332cef61396aac3f
ViNhan102 <slydemon06@gmail.com>
2025-04-24 08:27:23 +0700

    change config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/MvCShop/',
+    'assets' => '/MvCShop/assets/',
+    'baseURL' => 'http://localhost/MvCShop/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
